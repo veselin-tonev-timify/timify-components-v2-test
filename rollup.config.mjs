004b297766fabfedc59b26b3fadba141a4e537c4
Veselin Tonev <veselin.tonev@timify.com>
2025-08-20 09:47:41 +0300

    Update config by adding postcss

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -2,6 +2,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
 import dts from "rollup-plugin-dts";
+import postcss from 'rollup-plugin-postcss'
 
 import packageJson from "./package.json" assert { type: "json" };
 
@@ -28,6 +29,7 @@ export default [
         declaration: false,
         declarationDir: "dist/types"
       }),
+      postcss()
     ],
     external: ["react", "react-dom"]
   },
@@ -35,6 +37,6 @@ export default [
     input: "dist/types/index.d.ts",
     output: [{ file: "dist/index.d.ts", format: "esm" }],
     plugins: [dts()],
-    external: ["react", "react-dom"]
+    external: ["react", "react-dom", /.*\.css$/]
   },
 ];
